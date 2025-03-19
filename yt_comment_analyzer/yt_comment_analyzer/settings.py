f0fbee7944ecd3b14e4a7775469f7916e292b494
Sandesh-Pol <sandeshpol268@gmail.com>
2025-03-19 23:50:34 +0530

    Remove hardcoded YouTube API key from settings.py for security reasons

--- yt_comment_analyzer/yt_comment_analyzer/settings.py
+++ yt_comment_analyzer/yt_comment_analyzer/settings.py
@@ -136,10 +136,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-API_SERVICE_NAME = "youtube"
-API_VERSION = "v3"
-YOUTUBE_API_KEY = "AIzaSyBmkRo--2j9zLcyujQdhiGXnTvu_3M_TlI"  # Replace with your API key
+ # Replace with your API key
 
 
 
